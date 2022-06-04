8f701d53dd8390f699b2f1a32c79c0dcad1a2fd3
Niya Nicolova <niyannx@gmail.com>
2022-06-05 02:22:15 +0300

    Delete DOMIntroduction directory

--- DOMIntroduction/01.Edit Element/EditElement.js
+++
@@ -1,9 +0,0 @@
-function edit(element, match, replacer) {
-    let text = element.textContent;
-
-    const matcher = new RegExp(match, 'g');
-
-    let result = text.replace(matcher, replacer);
-
-    element.textContent = result;
-}
